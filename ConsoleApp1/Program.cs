fa5a3d0a1cb40a643a7e71457597c8332ebfdc11
sand0033 <saba@ou.edu>
2020-02-17 13:11:06 -0600

    list n arrays en class example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿//String Manipulation 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static string msg = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+          static void Main(string[] args)
+        {
+            Console.WriteLine(msg);
+
+            Console.WriteLine("What word would you like to look for? >>");
+            string lookupword = Console.ReadLine();
+            Console.WriteLine($"What word would you like to change {lookupword} to? >>");
+            string replaceword = Console.ReadLine();
+
+            bool doesContainWord = msg.Contains(lookupword);
+
+            if (doesContainWord == true)
+            {
+
+                //The sentence has the lookupword in it
+                string newSentence = msg.Replace(lookupword, replaceword);
+                Console.WriteLine(newSentence);
+            }
+            else
+            {
+                //The sentence DOES NOT have the lookupword in it 
+                Console.WriteLine($"Sorry, I could not find your word {lookupword}.");
+                { for (int i = lookupword.Length - 1; i >=0; i--)
+                    {
+                        Console.Write(lookupword[i]);
+                    }
+
+            }
+                Console.WriteLine(); 
+            Console.ReadKey();
+
+
+
+
+    }
+    }
+    }
+}
---
+++ Particpation Programming/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of type int with a size of 3 
+            int[] studentIds = new int[3];
+            string[] studentNames = { "Adam", "Katie", "Micah" }; // these two arrays are parallel, values at the same INDEX correlate to each other 
+
+
+            //      [0]      [1]       [2]
+            //[0][1][2]
+            studentIds[0] = 1;
+            studentIds[1] = 2;
+            studentIds[2] = 3;
+            // INDEX OUT OF RANGE BC WE ONLY HAVE SPACE FOR 3 VALUES -- studentIds[3] = 4;
+
+            Console.WriteLine($"Student with ID {studentIds[0]} has a name of {studentNames[0]}");
+            for (int i = 0; i < studentIds.Length; i++) // Could also do studentNames.Length as they are PARALLEL
+            {
+                Console.WriteLine($"Student with ID {studentIds[i]} has a name of {studentNames[i]}");
+            }
+            // DO THE SAME THING BUT WITH LISTS
+
+            List<int> studIds = new List<int>();
+            studIds.Add(4);
+            studIds.Add(5);
+            studIds.Add(6);
+            List<string> studNames = new List<string>() { "Lakin", "Grant", "Eric" };
+            //studNames.Add("Lakin");
+           // studNames.Add("Grant");
+           // studNames.Add("Eric");
+
+            for (int i = 0; i< studIds.Count; i++)
+            {
+                Console.WriteLine($"Student with ID {studIds[i]} has a name of {studNames[i]}");
+            }
+            //COMBINE ARRAY DATA AND LIST  INTO A DICTIONARY
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            //students.Add(1, "Adam");
+            for (int i = 0; i < studentIds.Length; i++) // Could also do studentNames.Length as they are PARALLEL
+            {
+                if (students.ContainsKey(studentIds[i]) ==  false)
+
+                    students.Add(studentIds[i], studentNames[i]));
+            }
+        if (students.ContainsKey(studIds[i]) == false)
+            (
+            students.Add(studIds[if], studNames[i]);
+            )
+        Console.WriteLine("MADE IT THRU ALIVE");
+                Console.ReadKey();
+
+        }
+    }
+}
