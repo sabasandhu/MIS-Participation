e55fec56f1cd850f2643a3394d3219eb6b71bf57
sand0033 <saba@ou.edu>
2020-03-11 12:51:00 -0500

    i can't access my participations from this computer

---
+++ Particpation Programming/ParticipationFunctions2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipationFunctions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+      static void (ParticpationMethod)
+
+        {
+            Console.WriteLine(" Which particiaption do you want to run? (e.g. FizzBuzz or MinMaxAvg ");
+            
+            Console.ReadKey(); 
+        }
+
+    internal class ParticpationMethod
+    {
+    }
+}
+}
